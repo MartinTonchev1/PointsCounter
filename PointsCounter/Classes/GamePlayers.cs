ca202a35c933feea2e9d8816a7050ad2e1ecbf1a
MartinTonchev1 <chushkopek1@abv.bg>
2023-04-25 20:12:28 +0300

    Add project files.

---
+++ PointsCounter/Classes/GamePlayers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointsCounter.Classes
+{
+    internal class GamePlayers
+    {
+        private string id;
+        private string gameId;
+        private string playerName;
+        private int playerPoints;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string GameId
+        {
+            get { return gameId; }
+            set { gameId = value; }
+        }
+        public string PlayerName
+        {
+            get { return playerName; }
+            set { playerName = value; }
+        }
+        public int PlayerPoints
+        {
+            get { return playerPoints; }
+            set { playerPoints = value; }
+        }
+    }
+}
